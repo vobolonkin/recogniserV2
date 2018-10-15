c200b254b8937e9e0eb8ddaabde60e8f11d229ed
Vladimir Obolonkin <vladimir.obolonkin@gmail.com>
2018-10-16 11:33:19 +1300

    Base functions done (see birdsLib) SDetctorNotes.Rmd -- axamples of use Nesx stages -- (1) to play with parameters (2) to involve learning

--- birdsLib.R
+++ birdsLib.R
@@ -16,7 +16,7 @@ getMel <- function(x) {
   
   tmp<-melfcc(
     x,
-    minfreq = 1000,
+    minfreq = 3000,
     maxfreq = 10000,
     fbtype = "mel",
     numcep = 12,
@@ -54,6 +54,8 @@ showMCPow<-function(
 }
 
 getAWins<-function(pov, thr=NULL ){
+## Action windows - start and stop times when the 'pov' is over threshold.
+## If thr=NULL -- the threshold is set to top quartile
   pov[is.nan(pov)]<-0
   mm=runmed(pov, k=kk)
   if(is.null(thr)) thr=quantile(mm, 0.75)
@@ -63,13 +65,36 @@ getAWins<-function(pov, thr=NULL ){
 
 inrange <- function(x, rr) {(x >= rr[1]) & (x <= rr[2])}
 
-winsWav <- function(infile=ifn, outfile=ofn, ww=action.wins[ii,], pad=seq(0, 0.5*samp.rate)){
+### Write selection table
+writeSelectionTable<-function (wins=wins, ofn="tmp.txt"){
+  header <- c("Selection",
+              "View",
+              "Channel",
+              "Begin Time (s)",
+              "End Time (s)")
+  cat(header, sep="\t", file=ofn)
+  cat("\n", file=ofn, append=T)
+  
+  df<-data.frame(sel=1:nrow(wins), View="Waveform 1", Channel=1, wins)
+  
+  write.table(df, file=ofn, sep="\t", col.names = F, append=T, quote=F, row.names = F)
+}
+
+winsWav <- function(infile=ifn, outfile=ofn, ww=action.wins[ii,], pad=seq(0, 0.5*samp.rate), win.wing=10*hopt){
   #pad -- space between recors, in samples
   snd<-pad
   
-  tmp<-apply(ww, 1, function(x) c(readWave(ifn, from=max(0,x[1]-10*hopt), to=x[2]+10*hopt, units = "sec")@left, pad))
+  tmp<-apply(ww, 1, function(x) c(readWave(ifn, from=max(0,x[1]-win.wing), to=x[2]+win.wing, units = "sec")@left, pad))
   snd<-c(pad, do.call("c",tmp))
   snd<-Wave(left=snd)
   writeWave(snd,filename = outfile , extensible = F)
+  
+  #Make and Write selection table for the fragments' file
+  ll=with(ww, end-start)
+  ds=length(pad)/samp.rate
+  tmp<-as.vector(rbind(ds, ll+2*win.wing))
+  tmp[2]<-tmp[2]-win.wing
+  wins<-data.frame(matrix(cumsum(tmp), byrow = T, ncol=2, dimnames=list(NULL, c("start", "end"))))
+  
+  writeSelectionTable(wins=wins, ofn=sub("\\.wav$", "SelectionTable.txt", outfile))
 }
-
